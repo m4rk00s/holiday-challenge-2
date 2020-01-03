8708615d64917a68058da488a229cc9b1e36ecaf
m4rk00s <marktony.x3.24@gmail.com>
2020-01-03 20:03:35 +0700

    setting heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { contactRoute } = require('./routes');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 4000;
 
 app.use(express.urlencoded({ extended: true }));
 
