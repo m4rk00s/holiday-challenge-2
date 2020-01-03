353d852fcfe971a1ccc08bd6941cbcb59f935db9
m4rk00s <marktony.x3.24@gmail.com>
2020-01-03 22:04:23 +0700

    add cors

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const { contactRoute } = require('./routes');
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/contacts', contactRoute);
 
