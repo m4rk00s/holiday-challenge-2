3cd0feb7b6b449231672dd48d1b13c85b7c31ed2
m4rk00s <marktony.x3.24@gmail.com>
2020-01-06 08:29:40 +0700

    seadanya

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
-const cors = require("cors");
-const ul = document.getElementById("contacts");
+const table = document.getElementById("contacts");
+const addContact = document.getElementById("add-contact")
 const url = "https://serene-tundra-64006.herokuapp.com/contacts";
 
-fetch(url, { mode: "no-cors" })
-  .then(response => console.log(response))
+function createNode(element) {
+  return document.createElement(element);
+}
+
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+// saat pertama kali website dibuka
+fetch(url)
+  .then(response => response.json())
+  .then(contacts => {
+    return contacts.map((contact, index) => {
+      const tr = createNode('tr');
+
+      let td = createNode('td');
+      td.textContent = index;
+      tr.appendChild(td);
+
+      td = createNode('td')
+      td.textContent = `${contact.first_name}`
+      tr.appendChild(td);
+
+      td = createNode('td');
+      td.textContent = `${contact.last_name}`
+      tr.appendChild(td);
+
+      td = createNode('td');
+      td.textContent = `${contact.phone}`
+      tr.appendChild(td);
+
+      td = createNode('td');
+      td.
+
+      table.appendChild(tr);
+    })
+  })
   .catch(error => console.error(error));
